1cd8ad0f2e1c652e5c257d371243093c2dc4b1af
Scott Buonya <scottbuonya@gmail.com>
2020-09-02 17:53:10 -0400

    made saving new notes work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
-var dbJson = require("Develop\db\db.json")
-
+var dbJson = require("./db/db.json")
+var fs = require("fs");
 var app = express();
 var PORT = process.env.PORT || 9001;
 
@@ -9,29 +9,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", function (res, req){
-    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+app.get("/", function (req, res){
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/notes", function (res, req){
-    res.sendFile(path.join(__dirname, "Develop/public/notes.html"))
+app.get("/notes", function (req, res){
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
 
 app.get("/api/notes", function(req, res){
     res.json(dbJson)
 });
 
-app.post("api/notes", function(req, res){
-    console.log(req, body)
+app.post("/api/notes", function(req, res){
+    var newNote = req.body;
+    dbJson.push(newNote)
+    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(dbJson), "utf8", function(err) {
+        if(err) throw err;
+        res.json(dbJson)
+    })
 });
 
-app.delete("api/notes/:id", function (req, res){
+app.delete("/api/notes/:id", function (req, res){
     res.send("Delete request at /user")
 });
 
-app.get("*", function (req, res){
-    res.sendFile(path.join(__dirname, index.html));
-});
+// app.get("*", function (req, res){
+//     res.sendFile(path.join(__dirname, index.html));
+// });
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
