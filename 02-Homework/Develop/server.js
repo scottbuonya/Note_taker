1599615e83fd9d916fe0b244202573420d734b81
Scott Buonya <scottbuonya@gmail.com>
2020-09-02 15:46:44 -0400

    got my dependecies, and starter for the server.js

---
+++ 02-Homework/Develop/server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 9001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function (res, req){
+    res.sendFile(path.join(_dirname, "Develop\public\index.html"));
+});
+
+app.get("/notes", function (res, req){
+    res.sendFile(path.join(_dirname, "Develop\public\notes.html"))
+})
