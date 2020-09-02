99090d6fa4010aec02723aa76dbfd3333f084070
Scott Buonya <scottbuonya@gmail.com>
2020-09-02 18:52:54 -0400

    added notes, and tested delete function

--- server.js
+++ server.js
@@ -20,23 +20,30 @@ app.get("/notes", function (req, res){
 app.get("/api/notes", function(req, res){
     res.json(dbJson)
 });
-
+// makes new notes and add it the the list
 app.post("/api/notes", function(req, res){
     var newNote = req.body;
+    newNote.id = dbJson.length + 1;
     dbJson.push(newNote)
     fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(dbJson), "utf8", function(err) {
         if(err) throw err;
         res.json(dbJson)
     })
 });
-
+// deletes notes
 app.delete("/api/notes/:id", function (req, res){
-    res.send("Delete request at /user")
+   const id = req.params._id;
+   const newNote = req.body;
+   newNote.remove(id,(err, newNote) =>{
+    if(err) throw err;
+    res.json(newNote)
+   });
+   
+    // req.params.id
+    // res.send("Delete request at /user")
 });
 
-// app.get("*", function (req, res){
-//     res.sendFile(path.join(__dirname, index.html));
-// });
+
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
